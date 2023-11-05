62ce85ac5d7c63521418c46a97233eb88a51e018
limye2 <144992260+limye2@users.noreply.github.com>
2023-11-05 23:36:52 +0900

    Initial Commit

---
+++ 09-5/main.c
@@ -0,0 +1,49 @@
+//
+//  main.c
+//  09-5
+//
+//  Created by MacBook Air on 2023/11/05.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void printMatrix(int A[][COLS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int A[ROWS][COLS] = {
+        {2, 3, 0},
+        {8, 9, 1},
+        {7, 0, 5}};
+    int B[ROWS][COLS]= {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}};
+    int C[ROWS][COLS];
+    
+    addMatrix(A, B, C);
+    printMatrix(C);
+    
+    return 0;
+}
+
